3b8dbb26ad32e48c1fdd296dc3d31de5da89823c
Dmitry Mingazov <mingazov@email.com>
2021-11-24 10:21:13 +0100

    Revert "Fix unused variables and wrong attributes"
    This reverts commit adb30d215d6147ab69c1217901b699e79e3603c2.

--- src/RequireAuth.js
+++ src/RequireAuth.js
@@ -1,17 +1,14 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { CircularProgress, Container } from '@mui/material';
 import { Box } from '@mui/system';
-import React from 'react';
+import React, { Component } from 'react';
 import { Navigate } from 'react-router-dom';
 
 const RequireAuth = ({ children }) => {
     const { isLoading, isAuthenticated } = useAuth0();
 
     return isLoading 
-        ? <Container 
-        // alignItems="center"
-        // justifyContent="center"
-        >
+        ? <Container>
             <Box 
         alignItems="center"
         justifyContent="center"
--- src/pages/Unauthorized.js
+++ src/pages/Unauthorized.js
@@ -14,6 +14,7 @@ const Unauthorized = () => {
         <Container>
             <Box 
                 sx={{ display: 'flex', flexDirection: 'column' }}
+                alignItems='center'
             >
                 <Typography variant="h3">
                     Unauthorized
