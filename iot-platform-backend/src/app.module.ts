b72d062815f1e8446e2ace43068c1344b243be7c
Sbyner <tmmscrltt@gmail.com>
2021-11-25 11:32:37 +0100

    Add basic config setup

--- iot-platform-backend/src/app.module.ts
+++ iot-platform-backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DevicesModule } from './devices.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot('mongodb://localhost/devices', {
       connectionName: 'devices',
     }),
