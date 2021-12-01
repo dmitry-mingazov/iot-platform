18c326a934a34c3a808c090f92b9a28c55332186
Luca <34515507+lucacervo98@users.noreply.github.com>
2021-12-01 16:13:58 +0100

    Add tests file for auth context

---
+++ src/__tests__/AuthContext.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { AuthStateContext } from "../components/context/AuthContext";
+import { useAuth0 } from "@auth0/auth0-react";
+import "@testing-library/jest-dom/extend-expect";
+
+jest.mock("@auth0/auth0-react");
+
+const renderComponent = (children) => {
+  render(<AuthStateContext>{children}</AuthStateContext>);
+};
+
+describe("Auth context", () => {
+  test("renders children components inside auth context", () => {
+    useAuth0.mockReturnValue({
+      isLoading: false,
+      isAuthenticated: true,
+      getAccessTokenSilently: jest.fn(() => new Promise(() => "tokenfinto")),
+    });
+    renderComponent(<div>Test Children</div>);
+    expect(screen.getByText("Test Children")).toBeInTheDocument();
+  });
+});
