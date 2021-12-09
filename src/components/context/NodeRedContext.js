755402e0513e7e678014d7e73eca6949a383a9fb
Dmitry Mingazov <mingazov@email.com>
2021-12-09 16:54:59 +0100

    Add api call to keep docker instance up

--- src/components/context/NodeRedContext.js
+++ src/components/context/NodeRedContext.js
@@ -2,6 +2,8 @@ import { useContext, useState, createContext, useEffect } from "react";
 import { AuthContext } from "./AuthContext";
 import NodeRedService from "../../services/NodeRedService";
 
+const getInstanceInterval = 10*1000;
+
 const NodeRedContext = createContext();
 const NodeRedStateContext = props => {
     const { token } = useContext(AuthContext);
@@ -12,6 +14,9 @@ const NodeRedStateContext = props => {
             NodeRedService.getInstance().then((instance) => {
                 setNodeRedUrl(process.env.REACT_APP_MANAGER_URL+':'+instance.port+'?access_token='+token);
             });
+            setInterval(() => {
+                NodeRedService.getInstance();
+            }, getInstanceInterval);
         }
     }, [token]);
 
