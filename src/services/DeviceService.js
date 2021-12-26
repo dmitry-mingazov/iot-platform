c50e756e494980ab14ed8c524641e87f5da25eda
Sbyner <tmmscrltt@gmail.com>
2021-12-26 11:33:04 +0100

    Change device service base path to /device

--- src/services/DeviceService.js
+++ src/services/DeviceService.js
@@ -1,7 +1,7 @@
 import HttpService from './HttpService';
 
 class DeviceService {
-    static baseUrl = `${process.env.REACT_APP_API_URL}/devices`;
+    static baseUrl = `${process.env.REACT_APP_API_URL}/device`;
 
     static async createDevice(device) {
         return HttpService.post(DeviceService.baseUrl, device);
