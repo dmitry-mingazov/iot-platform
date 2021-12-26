98a5b485ee297407e6b3e770c181ddb7b2a3f22b
Sbyner <tmmscrltt@gmail.com>
2021-12-26 11:37:43 +0100

    Differentiate single and multiple device API endpoints

--- src/services/DeviceService.js
+++ src/services/DeviceService.js
@@ -2,12 +2,13 @@ import HttpService from './HttpService';
 
 class DeviceService {
     static baseUrl = `${process.env.REACT_APP_API_URL}/device`;
+    static devicesUrl = `${DeviceService.baseUrl}s`;
 
     static async createDevice(device) {
         return HttpService.post(DeviceService.baseUrl, device);
     }
     static async getDevices() {
-        return HttpService.get(DeviceService.baseUrl);
+        return HttpService.get(DeviceService.devicesUrl);
     }
     static async getDevice(id) {
         return HttpService.get(`${DeviceService.baseUrl}/${id}`);
