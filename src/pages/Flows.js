0655902e0605fecf105aa0def04baf2e2edf192d
Luca <34515507+lucacervo98@users.noreply.github.com>
2021-12-27 18:42:44 +0100

    Update Flows page with the FlowsTable component

--- src/pages/Flows.js
+++ src/pages/Flows.js
@@ -1,24 +1,45 @@
 import { Typography } from "@mui/material";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../components/context/AuthContext";
 import { makeStyles } from "@mui/styles";
+import FlowsTable from "../components/FlowsTable";
 
 //Styles
 const useStyles = makeStyles({
   title: {
-    margin: 20,
+    marginTop: 20,
+    marginLeft: 60,
     fontWeight: "bold",
+  },
+  content: {
+    height: "580px",
+    marginRight: "60px",
+    marginLeft: "60px",
+    marginTop: "45px",
   },
 });
 
 function Flows() {
   const classes = useStyles();
   const { isTokenReady } = useContext(AuthContext);
+  const [rows, setRows] = useState([]);
 
-  // will run only on first render
   useEffect(() => {
     if (isTokenReady) {
-      //take flows
+      //GET FLOWS AND SET ROWS
+      setRows([
+        { id: 1, col1: "Hello", col2: "World", col3: "Device1, Device2" },
+        { id: 2, col1: "DataGrid", col2: "is Awesome", col3: "Device1" },
+        { id: 3, col1: "MUI", col2: "is Amazing", col3: "Device1, Device2" },
+        { id: 4, col1: "Ciao", col2: "is Amazing", col3: "Device1, Device2" },
+        { id: 5, col1: "Daje", col2: "is Amazing", col3: "Device1, Device2" },
+        { id: 6, col1: "Aloyaa", col2: "is Amazing", col3: "Device1, Device2" },
+        { id: 7, col1: "Aloyb", col2: "is Amazing", col3: "Device1, Device2" },
+        { id: 8, col1: "Aloyc", col2: "is Amazing", col3: "Device1, Device2" },
+        { id: 9, col1: "Aloyd", col2: "is Amazing", col3: "Device1, Device2" },
+        { id: 10, col1: "Aloyf", col2: "is Amazing", col3: "Device1, Device2" },
+        { id: 11, col1: "Aloytr", col2: "is Amazing", col3: "Device1" },
+      ]);
     }
   }, [isTokenReady]);
 
@@ -27,6 +48,9 @@ function Flows() {
       <Typography className={classes.title} variant="h4">
         Flows
       </Typography>
+      <div className={classes.content}>
+        <FlowsTable rows={rows} />
+      </div>
     </div>
   );
 }
