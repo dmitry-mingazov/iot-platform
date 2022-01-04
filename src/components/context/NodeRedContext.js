6349e352f4c29271908c93c5d91ddb9e8371a894
Luca <34515507+lucacervo98@users.noreply.github.com>
2022-01-04 16:16:21 +0100

    Update NodeRedContext with updateComment method (#71)

--- src/components/context/NodeRedContext.js
+++ src/components/context/NodeRedContext.js
@@ -3,95 +3,113 @@ import React from "react";
 import { AuthContext } from "./AuthContext";
 import NodeRedService from "../../services/NodeRedService";
 
-const getInstanceInterval = 10*1000;
+const getInstanceInterval = 10 * 1000;
 
 const NodeRedContext = createContext();
-const NodeRedStateContext = props => {
-    const { token } = useContext(AuthContext);
-    const [ nodeRedUrl, setNodeRedUrl ] = useState(null);
-    const [ flows, setFlows ] = useState([]);
-    const [ isNodeRedReady, setNodeRedReady ] = useState(false);
-    const [ isNodeRedLoading, setNodeRedLoading ] = useState(true);
-    const [ pushedIds, setPushedIds ] = useState({});
+const NodeRedStateContext = (props) => {
+  const { token } = useContext(AuthContext);
+  const [nodeRedUrl, setNodeRedUrl] = useState(null);
+  const [flows, setFlows] = useState([]);
+  const [isNodeRedReady, setNodeRedReady] = useState(false);
+  const [isNodeRedLoading, setNodeRedLoading] = useState(true);
+  const [pushedIds, setPushedIds] = useState({});
 
-    useEffect(() => {
-        if (token) {
-            NodeRedService.getInstance().then((instance) => {
-                setNodeRedUrl(instance.url);
-            });
-            setInterval(() => {
-                NodeRedService.getInstance();
-            }, getInstanceInterval);
-        }
-    }, [token]);
-
-    useEffect(() => {
-        if (nodeRedUrl) {
-            NodeRedService.getFlows(nodeRedUrl).then(_flows => {
-                setFlows(_flows);
-                setNodeRedReady(true);
-                setNodeRedLoading(false);
-            });
-        }
-    }, [nodeRedUrl])
-
-    const updatePushedIds = (deviceId, ids) => {
-        const newDeviceIds = pushedIds[deviceId] || {};
-        ids.forEach(id => {
-            newDeviceIds[id] = true;
-        });
-        const newPushedIds = pushedIds;
-        newPushedIds[deviceId] = newDeviceIds;
-        setPushedIds(newPushedIds);
+  useEffect(() => {
+    if (token) {
+      NodeRedService.getInstance().then((instance) => {
+        setNodeRedUrl(instance.url);
+      });
+      setInterval(() => {
+        NodeRedService.getInstance();
+      }, getInstanceInterval);
     }
+  }, [token]);
 
-    const getUniqueNodeIds = (deviceId, requiredIds) => {
-        const _ids = [];
-        const usedIds = {};
-        /* a node id is structured as `deviceId|counter`
+  useEffect(() => {
+    if (nodeRedUrl) {
+      NodeRedService.getFlows(nodeRedUrl).then((_flows) => {
+        setFlows(_flows);
+        setNodeRedReady(true);
+        setNodeRedLoading(false);
+      });
+    }
+  }, [nodeRedUrl]);
+
+  const updatePushedIds = (deviceId, ids) => {
+    const newDeviceIds = pushedIds[deviceId] || {};
+    ids.forEach((id) => {
+      newDeviceIds[id] = true;
+    });
+    const newPushedIds = pushedIds;
+    newPushedIds[deviceId] = newDeviceIds;
+    setPushedIds(newPushedIds);
+  };
+
+  const updateComment = (flowId, newComment) => {
+    const flowsToPost = flows.slice();
+    const commentNodeIndex = flowsToPost.findIndex((element) => {
+      if (element.z && element.type) {
+        return element.z === flowId && element.type === "comment";
+      } else return false;
+    });
+    if (commentNodeIndex !== -1) {
+      flowsToPost[commentNodeIndex].info = newComment;
+    }
+    NodeRedService.postFlows(nodeRedUrl, flowsToPost).then((_) => {
+      NodeRedService.getFlows(nodeRedUrl).then((_flows) => {
+        setFlows(_flows);
+      });
+    });
+  };
+
+  const getUniqueNodeIds = (deviceId, requiredIds) => {
+    const _ids = [];
+    const usedIds = {};
+    /* a node id is structured as `deviceId|counter`
          the following code gets all the ids matching the
          given deviceId, then saves their counter in the
          usedIds map <id, bool> */
-        flows
-            ?.map(flow => flow.id)
-            .filter(id => id.split('|')[0] === deviceId)
-            ?.map(id => parseInt(id.split('|')[1]))
-            ?.forEach(id => {usedIds[id] = true;});
-        let k = 0;
-        const devicePushedIds = pushedIds[deviceId] || [];
-        while(_ids.length < requiredIds) {
-            const curr = k++;
-            if(!usedIds[curr] && !devicePushedIds[curr]) {
-                _ids.push(curr);
-            }
-        }
-        updatePushedIds(deviceId, _ids);
-        return _ids.map(id => `${deviceId}|${id}`);
-    };
-
-    const value = {
-        nodeRedUrl,
-        getUniqueNodeIds,
-        isNodeRedReady,
-        isNodeRedLoading,
-        flows
+    flows
+      ?.map((flow) => flow.id)
+      .filter((id) => id.split("|")[0] === deviceId)
+      ?.map((id) => parseInt(id.split("|")[1]))
+      ?.forEach((id) => {
+        usedIds[id] = true;
+      });
+    let k = 0;
+    const devicePushedIds = pushedIds[deviceId] || [];
+    while (_ids.length < requiredIds) {
+      const curr = k++;
+      if (!usedIds[curr] && !devicePushedIds[curr]) {
+        _ids.push(curr);
+      }
     }
+    updatePushedIds(deviceId, _ids);
+    return _ids.map((id) => `${deviceId}|${id}`);
+  };
 
-    return (
-        <NodeRedContext.Provider
-            value={value}
-        >
-            {props.children}
-        </NodeRedContext.Provider>
-    )
-}
+  const value = {
+    nodeRedUrl,
+    getUniqueNodeIds,
+    isNodeRedReady,
+    isNodeRedLoading,
+    flows,
+    updateComment,
+  };
+
+  return (
+    <NodeRedContext.Provider value={value}>
+      {props.children}
+    </NodeRedContext.Provider>
+  );
+};
 
 function useNodeRed() {
-    const context = React.useContext(NodeRedContext);
-    if (context === undefined) {
-        throw new Error('useNodeRed must be used within a NodeRedStateContext');
-    }
-    return context;
+  const context = React.useContext(NodeRedContext);
+  if (context === undefined) {
+    throw new Error("useNodeRed must be used within a NodeRedStateContext");
+  }
+  return context;
 }
 
-export { useNodeRed, NodeRedStateContext,  };
+export { useNodeRed, NodeRedStateContext };
