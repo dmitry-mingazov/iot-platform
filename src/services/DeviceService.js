859389ceb2618c88049e59d4558f8c50f095dcdd
Luca <34515507+lucacervo98@users.noreply.github.com>
2022-01-24 21:29:24 +0100

    Update DeviceService with methods to delete one or more devices (#103)

--- src/services/DeviceService.js
+++ src/services/DeviceService.js
@@ -16,5 +16,11 @@ class DeviceService {
     static async getDevice(id) {
         return HttpService.get(`${DeviceService.baseUrl}/${id}`);
     }
+    static async deleteDevice(id) {
+        return HttpService.delete(`${DeviceService.baseUrl}/${id}`);
+    }
+    static async deleteDevices(devices) {
+        return HttpService.delete(DeviceService.baseUrl, devices);
+    }
 }
 export default DeviceService;
