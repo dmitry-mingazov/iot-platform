fe326b423b9dd0dd2256f7cb5951a92437691519
Dmitry Mingazov <mingazov@email.com>
2022-01-25 12:03:44 +0100

    Add UI dashboard to Node-RED instances (#97)

--- nodered-manager/create.sh
+++ nodered-manager/create.sh
@@ -9,6 +9,8 @@ done
 CONT_NAME=nodered-${user}
 NODE_DIR=$(pwd)/users/${user}
 
+IMAGE_NAME=iot-platform-nodered:1.0
+
 if [ -d "${NODE_DIR}" ]; then
     echo "${NODE_DIR} exists"
 else
@@ -16,8 +18,14 @@ else
     chown 1000:1000 ${NODE_DIR}
 fi
 
+if [ docker-inspect --type=image ${IMAGE_NAME} ]; then 
+    echo "${IMAGE_NAME} already existing"
+else
+    docker build -t ${IMAGE_NAME} .
+fi
+
 docker rm -f ${CONT_NAME}
 docker run -d --rm -p ${port}:1880 \
     -v ${NODE_DIR}:/data \
     --name ${CONT_NAME} \
-    nodered/node-red
+    ${IMAGE_NAME}
